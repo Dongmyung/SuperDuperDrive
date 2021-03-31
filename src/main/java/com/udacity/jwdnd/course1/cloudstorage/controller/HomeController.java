30da808da9e5b83efe723684e4a935af6a18229f
Dongmyung Ahn <crazyjumong@gmail.com>
2021-03-31 21:04:29 +0900

    Update HomeController.java

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    private final UserService userService;
+    private final FileService fileService;
+    private final NoteService noteService;
+    private final CredentialService credentialService;
+
+    public HomeController(UserService userService, FileService fileService, NoteService noteService, CredentialService credentialService) {
+        this.userService = userService;
+        this.fileService = fileService;
+        this.noteService = noteService;
+        this.credentialService = credentialService;
+    }
+
+    @GetMapping()
+    public String homeView(Authentication authentication, Model model) {
+        Integer userId = userService.getUser(authentication.getName()).getUserId();
+
+        model.addAttribute("userFiles", fileService.getFilesByUserId(userId));
+        model.addAttribute("userNotes", noteService.getNotesByUserId(userId));
+        model.addAttribute("userCredentials", credentialService.getCredentialsByUserId(userId));
+
+        return "home";
+    }
+}
