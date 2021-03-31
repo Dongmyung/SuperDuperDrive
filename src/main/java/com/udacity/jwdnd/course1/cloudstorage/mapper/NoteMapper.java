e9e3e7a37a65046d8cf6b3f20eb41fad05175078
Dongmyung Ahn <crazyjumong@gmail.com>
2021-03-31 21:00:40 +0900

    Add NoteMapper.java

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -14,14 +14,16 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
     UserNote getNoteByNoteId(Integer noteId);
 
-    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{title}, #{description}, #{userId})")
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId", keyColumn = "noteid")
     int saveNote(UserNote note);
 
-    @Update("UPDATE NOTES SET notetitle = #{title}, notedescription = #{description} WHERE noteid = #{noteId}")
-    void updateNote(UserNote note);
+    @Update("UPDATE NOTES SET notetitle = #{noteTitle}, notedescription = #{noteDescription} WHERE noteid = #{noteId} AND userid = #{userId}")
+    int updateNote(UserNote note);
 
     @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
     void deleteNoteByNoteId(Integer noteId);
 
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId} AND userid = #{userId}")
+    void deleteNoteByNoteIdAndUserId(Integer noteId, Integer userId);
 }
