28176ce2b3c1576b8eeb552f2491a4420ae3e214
Dongmyung Ahn <crazyjumong@gmail.com>
2021-03-31 21:01:10 +0900

    Add NoteController.java

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -0,0 +1,65 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.model.UserNote;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class NoteController {
+    private final NoteService noteService;
+    private final UserService userService;
+
+    public NoteController(NoteService noteService, UserService userService) {
+        this.noteService = noteService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/note/save")
+    public String savePost(@ModelAttribute UserNote userNote,
+                           Authentication authentication,
+                           RedirectAttributes redirectAttributes) {
+
+        Integer userId = userService.getUser(authentication.getName()).getUserId();
+        userNote.setUserId(userId);
+
+        try {
+            if (userNote.getNoteId() == null) {
+                noteService.saveNote(userNote);
+            } else {
+                noteService.updateNote(userNote);
+            }
+            redirectAttributes.addFlashAttribute("success", true);
+            redirectAttributes.addFlashAttribute("message", "Note saved successfully.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", true);
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+        }
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/note/delete/{noteId}")
+    public String deleteNote(@PathVariable("noteId") Integer noteId,
+                             Authentication authentication,
+                             RedirectAttributes redirectAttributes) {
+
+        Integer userId = userService.getUser(authentication.getName()).getUserId();
+
+        try {
+            noteService.deleteNoteByNoteIdAndUserId(noteId, userId);
+            redirectAttributes.addFlashAttribute("success", true);
+            redirectAttributes.addFlashAttribute("message", "Note deleted successfully.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", true);
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+        }
+
+        return "redirect:/home";
+    }
+
+}
