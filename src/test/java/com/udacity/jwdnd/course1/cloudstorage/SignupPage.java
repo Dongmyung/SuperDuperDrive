3d2a74f811b794d48a70bcc368a6b233ce5abcc0
Dongmyung Ahn <crazyjumong@gmail.com>
2021-04-01 09:58:48 +0900

    Add Page Object for Test at Signup Page

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/SignupPage.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+
+    @FindBy(id = "inputFirstName")
+    private WebElement inputFirstName;
+
+    @FindBy(id = "inputLastName")
+    private WebElement inputLastName;
+
+    @FindBy(id = "inputUsername")
+    private WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
+    private WebElement inputPassword;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+    public SignupPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void signup(String firstName, String lastName, String username, String password) {
+        this.inputFirstName.sendKeys(firstName);
+        this.inputFirstName.sendKeys(lastName);
+        this.inputFirstName.sendKeys(username);
+        this.inputFirstName.sendKeys(password);
+        this.submitButton.click();
+    }
+}
